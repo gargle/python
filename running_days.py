4b36aa70fa33222ab9cb964f5895bd530ee13379
gargle <johan.laenen@gmail.com>
2018-12-27 10:38:58 +0100

    Update running_days.py
    now with subtotals for each month

--- running_days.py
+++ running_days.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
 from datetime import date
-from calendar import isleap
+from calendar import isleap,monthrange
 
 # calculate the number of running days for a year (mondays, wednesday and fridays)
 
-print("year total")
+print("year  total  jan feb mar apr may jun jul aug sep oct nov dec")
 for year in range(2009,2039):
-    first_day = date(year,1,1).weekday()
-    running_days = sum(list(map(lambda y: 1+((366 if isleap(year) else 365)-y)//7,
-                                map(lambda x: 7 if (x==0) else x,
-                                    [(8-first_day)%7,  # first monday
-                                     (10-first_day)%7, # first wednesday
-                                     (12-first_day)%7  # first friday
-                                    ]
-                                ))))
-    print("%4d  %3d" % (year, running_days))
+    running_days = []
+    for month in range(1,13):
+        (first_day, total_days) = monthrange(year,month)
+        running_days.append(sum(list(map(lambda y: 1+(total_days-y)//7,
+                                      map(lambda x: 7 if (x==0) else x,
+                                          [(8-first_day)%7,  # first monday
+                                           (10-first_day)%7, # first wednesday
+                                           (12-first_day)%7  # first friday
+                                          ]
+                                      )))))
+    print("%4d   %3d  " % (year, sum(running_days)),end='')
+    for month in range(0,12):
+        print("  %2d" % running_days[month], end='')
+    print("")
