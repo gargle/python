41ad24881be1b4203c4098431617b588b5e3713a
Johan Laenen <johan.laenen@bpost.be>
2023-02-16 10:38:08 +0100

    rewrite blog2tcx.py with black

--- blog2tcx.py
+++ blog2tcx.py
@@ -5,26 +5,30 @@ import re
 from datetime import datetime, timedelta
 import os
 
-url = feedparser.parse('./blog-02-15-2023.xml')
+url = feedparser.parse("./blog-02-15-2023.xml")
 
 totals = {}
 for item in url.entries:
-    if re.search(r'^(Machelen|Berbroek)', item.title):
+    if re.search(r"^(Machelen|Berbroek)", item.title):
         # item.published is our endtime
         published = datetime.fromisoformat(item.published)
-        match = re.search(r'\d{4}-\d{2}-\d{2}', item.published)
-        if (published.date().year < 2020):
+        match = re.search(r"\d{4}-\d{2}-\d{2}", item.published)
+        if published.date().year < 2020:
             continue
-        content = item.content[0]['value']
-        match = re.search(r'(\d+[.,]?\d*)\skm', content)
-        distance = float(match.group(1))*1000
-        match = re.search(r',\s(\d+)h(\d+)m(\d+)s,', content)
+        content = item.content[0]["value"]
+        match = re.search(r"(\d+[.,]?\d*)\skm", content)
+        distance = float(match.group(1)) * 1000
+        match = re.search(r",\s(\d+)h(\d+)m(\d+)s,", content)
         if match is not None:
-            runningTime=int(match.group(1))*3600+int(match.group(2))*60+int(match.group(3))
+            runningTime = (
+                int(match.group(1)) * 3600
+                + int(match.group(2)) * 60
+                + int(match.group(3))
+            )
         else:
-            match = re.search(r',\s(\d+)m(\d+)s,', content)
+            match = re.search(r",\s(\d+)m(\d+)s,", content)
             if match is not None:
-                runningTime=int(match.group(1))*60+int(match.group(2))
+                runningTime = int(match.group(1)) * 60 + int(match.group(2))
             else:
                 print("ERROR")
                 print(item.published)
@@ -32,38 +36,37 @@ for item in url.entries:
                 exit(1)
         filename = "run-%s.tcx" % (item.published)
         print(filename)
-        endTime = published - timedelta(days=0,hours=0,minutes=30)
+        endTime = published - timedelta(days=0, hours=0, minutes=30)
         startTime = endTime - timedelta(hours=0, minutes=0, seconds=runningTime)
         if os.path.isfile(filename):
-            print(filename,"exists already!")
+            print(filename, "exists already!")
             exit(1)
         f = open(filename, "w")
-        f.write('<?xml version="1.0" encoding="UTF-8"?><TrainingCenterDatabase xsi:schemaLocation="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd" xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns3="http://www.garmin.com/xmlschemas/ActivityExtension/v2">')
+        f.write(
+            '<?xml version="1.0" encoding="UTF-8"?><TrainingCenterDatabase xsi:schemaLocation="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd" xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns3="http://www.garmin.com/xmlschemas/ActivityExtension/v2">'
+        )
         f.write("\n")
-        f.write('<Activities>'+"\n")
-        f.write('<Activity Sport="running">'+"\n")
-        f.write('<Id>'+str(startTime.isoformat())+'</Id>'+"\n")
-        f.write('<Lap StartTime="'+str(startTime.isoformat())+'">'+"\n")
-        f.write('<TotalTimeSeconds>'+str(runningTime)+'</TotalTimeSeconds>'+"\n")
-        f.write('<DistanceMeters>'+str(distance)+'</DistanceMeters>'+"\n")
-        f.write('<TriggerMethod>Manual</TriggerMethod>'+"\n")
-        f.write('<Track>'+"\n")
-        f.write('<Trackpoint>'+"\n")
-        f.write('<Time>'+str(startTime.isoformat())+'</Time>'+"\n")
-        f.write('<DistanceMeters>0</DistanceMeters>'+"\n")
-        f.write('</Trackpoint>'+"\n")
-        f.write('<Trackpoint>'+"\n")
-        f.write('<Time>'+str(endTime.isoformat())+'</Time>'+"\n")
-        f.write('<DistanceMeters>'+str(distance)+'</DistanceMeters>'+"\n")
-        f.write('</Trackpoint>'+"\n")
-        f.write('</Track>'+"\n")
-        f.write('</Lap>'+"\n")
-        f.write('</Activity>'+"\n")
-        f.write('</Activities>'+"\n")
-        f.write('</TrainingCenterDatabase>'+"\n")
+        f.write("<Activities>" + "\n")
+        f.write('<Activity Sport="running">' + "\n")
+        f.write("<Id>" + str(startTime.isoformat()) + "</Id>" + "\n")
+        f.write('<Lap StartTime="' + str(startTime.isoformat()) + '">' + "\n")
+        f.write("<TotalTimeSeconds>" + str(runningTime) + "</TotalTimeSeconds>" + "\n")
+        f.write("<DistanceMeters>" + str(distance) + "</DistanceMeters>" + "\n")
+        f.write("<TriggerMethod>Manual</TriggerMethod>" + "\n")
+        f.write("<Track>" + "\n")
+        f.write("<Trackpoint>" + "\n")
+        f.write("<Time>" + str(startTime.isoformat()) + "</Time>" + "\n")
+        f.write("<DistanceMeters>0</DistanceMeters>" + "\n")
+        f.write("</Trackpoint>" + "\n")
+        f.write("<Trackpoint>" + "\n")
+        f.write("<Time>" + str(endTime.isoformat()) + "</Time>" + "\n")
+        f.write("<DistanceMeters>" + str(distance) + "</DistanceMeters>" + "\n")
+        f.write("</Trackpoint>" + "\n")
+        f.write("</Track>" + "\n")
+        f.write("</Lap>" + "\n")
+        f.write("</Activity>" + "\n")
+        f.write("</Activities>" + "\n")
+        f.write("</TrainingCenterDatabase>" + "\n")
         f.close()
 
 exit(1)
-
-
-
