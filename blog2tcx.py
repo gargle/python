b29a0309abf29ce5a91d1ac7f5f6e568b2d4bad3
Johan Laenen <johan.laenen@bpost.be>
2023-02-17 06:42:53 +0100

    tem 17/02/2023

--- blog2tcx.py
+++ blog2tcx.py
@@ -7,14 +7,23 @@ import os
 
 url = feedparser.parse("./blog-02-15-2023.xml")
 
-totals = {}
+f = open("all.tcx", "w")
+f.write(
+            '<?xml version="1.0" encoding="UTF-8"?><TrainingCenterDatabase xsi:schemaLocation="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd" xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns3="http://www.garmin.com/xmlschemas/ActivityExtension/v2">'
+)
+f.write("\n")
+f.write("<Activities>" + "\n")
+
 for item in url.entries:
     if re.search(r"^(Machelen|Berbroek)", item.title):
         # item.published is our endtime
         published = datetime.fromisoformat(item.published)
         match = re.search(r"\d{4}-\d{2}-\d{2}", item.published)
-        if published.date().year < 2020:
+        if published.date().year < 2019 or published.date().year > 2019:
             continue
+        #if published.date().month < 2:
+        #    continue
+        print(published.date())
         content = item.content[0]["value"]
         match = re.search(r"(\d+[.,]?\d*)\skm", content)
         distance = float(match.group(1)) * 1000
@@ -34,19 +43,8 @@ for item in url.entries:
                 print(item.published)
                 print(content)
                 exit(1)
-        filename = "run-%s.tcx" % (item.published)
-        print(filename)
         endTime = published - timedelta(days=0, hours=0, minutes=30)
         startTime = endTime - timedelta(hours=0, minutes=0, seconds=runningTime)
-        if os.path.isfile(filename):
-            print(filename, "exists already!")
-            exit(1)
-        f = open(filename, "w")
-        f.write(
-            '<?xml version="1.0" encoding="UTF-8"?><TrainingCenterDatabase xsi:schemaLocation="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd" xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns3="http://www.garmin.com/xmlschemas/ActivityExtension/v2">'
-        )
-        f.write("\n")
-        f.write("<Activities>" + "\n")
         f.write('<Activity Sport="running">' + "\n")
         f.write("<Id>" + str(startTime.isoformat()) + "</Id>" + "\n")
         f.write('<Lap StartTime="' + str(startTime.isoformat()) + '">' + "\n")
@@ -65,8 +63,9 @@ for item in url.entries:
         f.write("</Track>" + "\n")
         f.write("</Lap>" + "\n")
         f.write("</Activity>" + "\n")
-        f.write("</Activities>" + "\n")
-        f.write("</TrainingCenterDatabase>" + "\n")
-        f.close()
+
+f.write("</Activities>" + "\n")
+f.write("</TrainingCenterDatabase>" + "\n")
+f.close()
 
 exit(1)
